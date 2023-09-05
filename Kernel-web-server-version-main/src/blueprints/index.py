a854f68cde6d3f9cde746dbcfcd3846004228be5
Abolfazl <dahaghaynhmh@gmail.com>
2023-09-05 08:29:08 +0330

    Fork updateded kernel added

--- Kernel-web-server-version-main/src/blueprints/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import jsonify
-index = Blueprint('index', __name__)
-
-@index.route('/', methods=['GET'])
-def index_func():
-    # this API used to check if the server is running
-    return jsonify({"message":"Welcome, server is running"}), 200
