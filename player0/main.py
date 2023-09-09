7b7561bc579b8efb9b32ae51e59c0ab9b3c7c577
IMAN <95289929+I-MA-N@users.noreply.github.com>
2023-09-09 11:44:20 +0330

    adding some comments

--- player0/main.py
+++ player0/main.py
@@ -1,6 +1,6 @@
 import random
 from src.game import Game
-VARS = {"strategic_troops_number":8 , "mytroops/enemytroops (beta)" : 1.2 , "beta_plus": 1.5, "TroopsTunnel" : 3}
+VARS = {"strategic_troops_number":8 , "mytroops/enemytroops (beta)" : 1.2 , "beta_plus": 1.5, "TroopsTunnel" : 3 , "number_of_attack_attemps" : 3 , "troops_to_put_on_strategics" : 3}
 flag = False
 
 ListOfTunnels = []
@@ -171,37 +171,44 @@ def turn(game):
     beta_plus = VARS["beta_plus"]
 
     for enemy in strategic_nodes:  
-        enemy_troops_on_node = int(number_of_troops[str(enemy)]) + int(number_of_fort_troops[str(enemy)]) #getting the number of enemy troops on the strategic node
         if owner[str(enemy)] != my_id:
-            for my in adjacents[str(enemy)]:
+            enemy_troops_on_node = int(number_of_troops[str(enemy)]) + int(number_of_fort_troops[str(enemy)]) #getting the number of enemy troops on the strategic node
+            for my in adjacents[str(enemy)]:    
                 if owner[str(my)] == my_id:
                     my_troops_layer1node = number_of_troops[str(my)]
-                    attack_score = (my_remaining_troops-3 + my_troops_layer1node)/ enemy_troops_on_node
-                    if attack_score > beta:
+                                                        #این 3 رو برای این کم کردم که بعد میخوام دوتا اضافه کنم سرباز داشته باشیم و ارور نگیریم ولی نمیدونم چرا همون دو نذاشتم باشه و سه گذاشتم یادم نمیاد..
+                    attack_score = (my_remaining_troops-3 + my_troops_layer1node)/enemy_troops_on_node
+                    if attack_score > beta: #اگر امتیاز حمله از بتا بزرگ تر بود میاد به دیکشنری با مشخصات مورد نیاز اضافه میکنه در غیر این صورت که نیازی نیست این کار رو بکنه چون سرباز نداریم
                         opurtunity_of_attacks [(my , enemy)] = {'attack_score':attack_score , 'enemy_troops' : enemy_troops_on_node, 'my_troops_layer1node':my_troops_layer1node , 'attackon' : False}
                         #s is enemy node and node is my node  #the chance of attacking #the number of enemy troops on strategic node #the troops i have on layer1 before 
-
+                                                #چرا کلید رو تاپل دادم؟ چون اگر هر کدوم از سیاره های خودمون یا اونارو میدادم تکراری توش پیدا میشد و حذف میکرد...
 
     #start putting troops on suitable nodes #جهت احتیاط این شرط رو گذاشتم که یه چند تا سربازی هم داشته باشیم تا جاهای دیگه به عنوان مدافع کار بذاریم!
-    if len(opurtunity_of_attacks) >= 1 and my_remaining_troops > 2:
+    if len(opurtunity_of_attacks) >= 1:
         #sorting the chance of attacking in the dictionary from a high amount to a low one
         sort_chance_of_attacks = sorted(opurtunity_of_attacks.items(), key = lambda item: item[1]['attack_score'] , reverse=True)
         for n in sort_chance_of_attacks:
             n_defenders = n[1]['enemy_troops']
-            num_of_needed_troops = (int(-(-(beta*n_defenders)//1))+2) - n[1]['my_troops_layer1node']
-            if my_remaining_troops > num_of_needed_troops > 0 :
-                print (game.put_troop(n[0][0] , num_of_needed_troops))#put n troops on the best choice of attacking, which n has been rounded up to ensure that the attack will be successful!
-                n[1]['attackon'] = True
-                my_remaining_troops -= num_of_needed_troops
-            else:
-                n[1]['attackon'] = True
-    #if there isn't any suitable node to put troops on we should put our troops on the nodes that we have now or the nodes which no one own them randomly!
-    list_of_my_strategics = [] 
+                                    #توضیح فرمول و مشکلش سر اینکه 5 تا سرباز تو خونه دشمن باشه! که میاد و دو بار حمله رو ادامه میده اما در بقیه حالات تقریبا میشه گفت همش سه بار حمله میکنه
+            num_of_needed_troops = (int(-(-(beta*n_defenders)//1))+ VARS['number_of_attack_attemps']-1) - n[1]['my_troops_layer1node']
+            #if "num_of_needed_troops" becomes a number which isn't larger than zero I don't need to put any troops of node because I have enough troops
+            if my_remaining_troops >= num_of_needed_troops: #برای بهینه تر شدن این شرط ها نظری دارید؟
+                                        #if this condition isn't checked, it may raises and error about that "num_of_needed_troops" is zero or a negetive number
+                if num_of_needed_troops > 0:
+                    print (game.put_troop(n[0][0] , num_of_needed_troops))#put n troops on the best choice of attacking, which n has been rounded up to ensure that the attack will be successful!
+                    n[1]['attackon'] = True #it is written to use in the attack state
+                    my_remaining_troops -= num_of_needed_troops #it is here to update the troops that i have each time
+                else:  
+                    n[1]['attackon'] = True
+    #if there isn't any suitable node to put troops on we should put our troops on the strategic nodes
+    list_of_my_strategics = [] #پیشنهاد: میشه این رو به یه دیکشنری تبدیل کرد که بیاد بر اساس تعداد سرباز موجود توش از کوچک به بزرگ مرتب بشه و سرباز های باقی مونده رو به سیاره هایی که کمترین سرباز رو دارند اضافه کنه
     for x in strategic_nodes:
-        if owner[str(x)] == my_id and game.get_number_of_troops_to_put()['number_of_troops'] > 2:
+        if owner[str(x)] == my_id:
             list_of_my_strategics.append(int(x))
-    while game.get_number_of_troops_to_put()['number_of_troops'] > 2:
-        print (game.put_troop(random.choice(list_of_my_strategics), random.choice([2,3])))
+
+    while my_remaining_troops > 2:
+        my_remaining_troops -= VARS['troops_to_put_on_strategics']
+        print (game.put_troop(random.choice(list_of_my_strategics[0]), VARS['troops_to_put_on_strategics']))
 
     print(game.next_state()) #going to the next state
 
@@ -213,11 +220,11 @@ def turn(game):
     number_of_fort_troops = game.get_number_of_fort_troops()
     
     if len(sort_chance_of_attacks) >= 1:
-        for on in sort_chance_of_attacks:
+        for on in sort_chance_of_attacks:     #ممکنه به یه سیاره بخوایم دو بار حمله کنیم! و بار اول مال ما بشه...
             if on[1]['attackon'] == True and game.get_owners()[str(on[0][1])] != my_id and game.get_number_of_troops()[str(on[0][0])] > 1:
-                print (game.attack(on[0][0] , on[0][1] , beta , 0.7))
+                print (game.attack(on[0][0] , on[0][1] , beta , 0.7))                      #تو همون دوبار حمله کردنه تعداد سرباز هامون کم میشه دیگه پس باید چکش کنیم
     
-    random_attacks = {}
+    random_attacks = {} #توضیح این بخش کد با پینت
     for mine in owner:
         if owner[mine] == my_id:
             for enemies in adjacents[mine]:
@@ -229,7 +236,7 @@ def turn(game):
                         random_attacks[(int(mine) , enemies)] = {'attack_score':attack_score , 'enemy_troops' : enemy_troops_on_node, 'my_troops_layer1node':my_troops_layer1node , 'attackon' : False}
     random_attacks = sorted(random_attacks.items(), key = lambda item: item[1]['attack_score'] , reverse=True)
     for each_attack in random_attacks:
-        if game.get_number_of_troops()[each_attack[0][0]] > 1:
+        if game.get_number_of_troops()[each_attack[0][0]] > 1: #برای اینکه ممکنه چند بار یه سیاره ما به سیار های دیگه حمله کنه...
             print (game.attack(each_attack[0][0] , each_attack[0][1] , beta , 0.5))
     
     print(game.next_state())
@@ -270,6 +277,6 @@ def turn(game):
             if owner[str(stra)] == my_id and number_of_troops[str(stra)] > troops_in:
                 troops_in = number_of_troops[str(stra)]
                 max_node = stra
-        print (game.fort(stra , troops_in-1))
+        print (game.fort(max_node , troops_in-1))
         flag = True
     
