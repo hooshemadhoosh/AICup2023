1d841aa155605eaca0e599aa1723e32f08928d77
IMAN <95289929+I-MA-N@users.noreply.github.com>
2023-09-25 14:17:15 +0330

    a

--- player0/main.py
+++ player0/main.py
@@ -316,7 +316,7 @@ def turn(game: Game):
             enemy_maxi_id = i
     print ('number of my troops:' , my_remaining_troops)
     my_remaining_troops = game.get_number_of_troops_to_put()['number_of_troops']
-    if mini < 30 and enemy_maxi > 14 and count_our_stra < 3 and turn_number < 116:
+    if mini < 30 and enemy_maxi > 14 and count_our_stra < 3 and turn_number < 116 and my_remaining_troops:
         if my_remaining_troops > reinforcment_soldiers:
             print (game.put_troop(mini_id , reinforcment_soldiers))
             number_of_troops[str(mini_id)] += reinforcment_soldiers
